c688851e4bb73d007cd9a07a99a6fe705afd401c
pratik2061 <prayiksharma23@gmail.com>
2024-06-11 13:49:37 +0545

    🔥 project got completed

--- js/bag.js
+++ js/bag.js
@@ -4,6 +4,7 @@ onload()
 function onload(){
     LoadBagItemsObject();
     displayBagItems();
+    displayBagSummary();
 }
 function LoadBagItemsObject(){
     BagItemObject = BagItems.map(ItemId => {
@@ -46,11 +47,60 @@ function GenerateItemHtml(item){
               </div>
             </div>
 
-            <div class="remove-from-cart">X</div>
+            <div class="remove-from-cart" onclick="removefrombag(${item.id})">X</div>
            </div>
     `;
 }
-let removeBtn = document.querySelector(".remove-from-cart")
-removeBtn.addEventListener("click" , ()=>{
+// let removeBtn = document.querySelector(".remove-from-cart")
+// removeBtn.addEventListener("click" , ()=>{
     
-})
+// })
+
+function removefrombag(ItemId){
+BagItems = BagItems.filter(BagItemId =>BagItemId != ItemId )  
+ localStorage.setItem('BagItems' , JSON.stringify(BagItems))
+ LoadBagItemsObject();
+ displayBagItems();
+ DisplayBagIcon();
+ displayBagSummary()
+}
+ function displayBagSummary(){
+  let BagSummaryElement = document.querySelector(".bag-summary")
+  let totalItems = BagItemObject.length
+  let totalMrp = 0
+  let totalDiscount = 0
+  let finalPayment = 0 
+  const Convenience_fee = 99
+
+  BagItemObject.forEach(bagItem => {
+    totalMrp += bagItem.original_price
+    totalDiscount += bagItem.original_price - bagItem.current_price;    
+  });
+  finalPayment = totalMrp - totalDiscount + Convenience_fee
+  BagSummaryElement.innerHTML = `
+            <div class="bag-details-container">
+            <div class="price-header">PRICE DETAILS (${totalItems} items) </div>
+            <div class="price-item">
+              <span class="price-item-tag">Total MRP</span>
+              <span class="price-item-value">Rs ${totalMrp}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Discount on MRP</span>
+              <span class="price-item-value priceDetail-base-discount">Rs ${totalDiscount}</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Convenience Fee</span>
+              <span class="price-item-value">Rs 99</span>
+            </div>
+            <hr>
+            <div class="price-footer">
+              <span class="price-item-tag">Total Amount</span>
+              <span class="price-item-value">Rs ${finalPayment}</span>
+            </div>
+          </div>
+          <button class="btn-place-order">
+            <div class="css-xjhrni">PLACE ORDER</div>
+          </button>
+   
+   `
+}
